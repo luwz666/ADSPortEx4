d0433892552809f21b16cb13164b8459bbab2a30
55149499 <55149499@ASC-ACA9375>
2025-09-21 21:15:49 +0100

    Add project files.

---
+++ ADSPortEx4/Station.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADSPortEx4
+{
+    class Station : IComparable
+    {
+        public Station(string name, int passengerCapacity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Name
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public int PassengerCapacity
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public int CompareTo(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
